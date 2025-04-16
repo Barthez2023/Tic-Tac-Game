ef098ae001525ee4ca64e5b982d726d2c4bf2db3
Barthezpro <bartheztonlio@gmail.com>
2025-04-17 01:44:38 +0300

    Le jeu Tic-Tac

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Ce ripo resente le jeu de tic-tac
 # mercredi 16 avril
 J'ai commencer le jeu tic-tac j'ai juste creer le partie visuel du jeu et placer les different boutton necessaire
+# jeudi 17 avril
+le jeu tic-tac est terminer maintenenant je vais l'inplementer dans Netbeans et l'ajoute rplusieurs autre boutton pour facilite son utilisation
--- Tic-Tac/Game.java
+++ Tic-Tac/Game.java
@@ -18,6 +18,9 @@ public class Game {
     String joueurX="X";
     String joueurO="O";
     String joueurActuel=joueurX;
+
+    boolean gameover=false;
+    int turns;
     public Game(){
         frame.setVisible(true);
         frame.setSize(boardWith,boardheight);
@@ -58,8 +61,110 @@ public class Game {
                 tile.setFont(new Font("Arial",Font.BOLD,120));
                 tile.setFocusable(false);
                 //tile.setText(joueurActuel);
+                tile.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent evt){
+                        if(gameover) return;                      //permet de ne pas donner la permission de changer la va leur dans les cases
+                        JButton tile= (JButton)evt.getSource();    /*getSource() sert à identifier l’objet qui a déclenché l'événement. etant donne que l'on a plusieur boutton on utilise getSource() pour retrouver exactement lequel a été cliqué.*/
+                        if (tile.getText()=="") {     //optionnel permet de ne pas jouer de facon infini
+                            tile.setText(joueurActuel);
+                            turns++;
+                            CheckVainqueur();
+                            if (!gameover) {
+                                if (joueurActuel==joueurX) {
+                                    joueurActuel=joueurO;
+                                }
+                                else
+                                    joueurActuel=joueurX;
+                                //avec l'operateur ternaire on a:joueurActuel=joueurActuel==joueurX?joueurO:joueurX
+                                text.setText(joueurActuel+ "'s turn.");
+                            }
+                        }
+                        
+                    }
+                });
             }
             
         }
+    }
+    private void CheckVainqueur(){
+        //on va verifier si soit les "X" ou les "O" on reussir a former une ligne horizontale alors celui la  sera le vainqueur
+        for (int i = 0; i < boutton.length; i++) {
+            if (boutton[i][0].getText() == ""){
+                continue;
+            }
+            if (boutton[i][0].getText()==boutton[i][1].getText() && boutton[i][1].getText()==boutton[i][2].getText()) {
+                for (int j = 0; j < boutton.length; j++) {
+                    setVainqueur(boutton[i][j]);
+                }
+                gameover=true;
+                return;
+            }
+        }
+        //on va verifier si soit les "X" ou les "O" on reussir a former une ligne verticale alors celui la  sera le vainqueur
+        for (int i = 0; i < boutton.length; i++) {
+            if (boutton[0][i].getText() == ""){
+                continue;
+            }
+            if (boutton[0][i].getText()==boutton[1][i].getText() && boutton[1][i].getText()==boutton[2][i].getText()) {
+                for (int j = 0; j < boutton.length; j++) {
+                    setVainqueur(boutton[j][i]);
+                }
+                gameover=true;
+                return;
+            }
+        }
+
+       //on va verifier si soit les "X" ou les "O" on reussir a former une ligne diagonale principale alors celui la  sera le vainqueur
+        if (boutton[0][0].getText().equals(boutton[1][1].getText()) && boutton[1][1].getText().equals(boutton[2][2].getText()) && !boutton[0][0].getText().equals("")) {
+            for (int j = 0; j < boutton.length; j++) {
+                setVainqueur(boutton[j][j]);
+            }
+            gameover=true;
+            return;
+        }
+
+        //on va verifier si soit les "X" ou les "O" on reussir a former une ligne diagonale secondaire alors celui la  sera le vainqueur
+        if (boutton[2][0].getText().equals(boutton[1][1].getText()) && boutton[1][1].getText().equals(boutton[0][2].getText()) && !boutton[2][0].getText().equals("")) {
+            int i=2;
+            for (int j = 0; j < boutton.length; j++) {
+                setVainqueur(boutton[i][j]);
+                i--;
+            }
+            gameover=true;
+            return;
+        }
+
+       /*//on va verifier si soit les "X" ou les "O" on reussir a former une ligne diagonale secondaire alors celui la  sera le vainqueur
+        if (boutton[2][0].getText()==boutton[1][1].getText() && boutton[1][1].getText()==boutton[0][2].getText()) {
+            int i=2;
+            for (int j = 0; j < boutton.length; j++) {
+                setVainqueur(boutton[i][j]);
+                i--;
+            }
+            gameover=true;
+            return;
+        }*/
+
+        if (turns==9) {
+            for (int i = 0; i < boutton.length; i++) {
+                for (int j = 0; j < boutton.length; j++) {
+                    setTile(boutton[i][j]);
+                }
+            }
+            gameover=true;
+        }
+    }
+    private void setVainqueur(JButton tile){
+        tile.setForeground(Color.GREEN);
+        tile.setBackground(Color.gray);
+        text.setText(joueurActuel +" is the Winner");
+        text.setForeground(Color.GREEN);
+    
+    }
+    private void setTile(JButton tile){
+        tile.setForeground(Color.red);
+        tile.setBackground(Color.gray);
+        text.setText("Game Over trie again!!!");
+
     }
 }
